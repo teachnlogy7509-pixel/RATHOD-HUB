906ae43253d0dea063d205a8b4b4dff97c411c2f
teachnlogy7509-pixel <teachnlogy7509@gmail.com>
2026-09-13 21:32:16 -0400

    Refresh app cache for collection feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rathod-hub-v14';
+const CACHE_NAME = 'rathod-hub-v15';
 const APP_SHELL = [
   './',
   './index.html',
@@ -6,7 +6,8 @@ const APP_SHELL = [
   './icon-192.png',
   './icon-512.png',
   './rathod-modern-theme.css',
-  './rathod-modern-effects.js'
+  './rathod-modern-effects.js',
+  './student-collection.js'
 ];
 
 self.addEventListener('install', event => {
